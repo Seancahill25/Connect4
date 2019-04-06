80d0d87b91a119c8a0861619acc55b87e9ba4152
Sean Cahill <seancahill25@gmail.com>
2019-04-05 23:40:08 -0400

    changed file names

---
+++ Connect4 v3/Connect4v3/Game.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawBoardV2Milo
+{
+    public class Game
+    {
+        public char[,] GameMoves(int height , int width)
+        {
+            char[,] moves = new char[height, width];
+            return moves;
+        }
+       
+        public void DropPiece(char[,] moves)
+        {
+            Console.WriteLine("Please enter a number between 1 and 7: ");
+            int dropChoice = Convert.ToInt32(Console.ReadLine());
+            int height = 4;
+
+            if(moves[height, dropChoice] != 'X')
+            {
+                moves[height, dropChoice] = 'X';
+            }
+
+        }
+    }
+}
